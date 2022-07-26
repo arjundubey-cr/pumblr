64c0bf982dfe53cb2c2da409e566eb9c712030ba
Arjun Dubey <arjundubeycr@gmail.com>
2022-07-26 23:16:31 +0530

    client setup

--- server/index.js
+++ server/index.js
@@ -1,4 +1,30 @@
-import express from 'express'
-import bodyParser from 'body-parser'
-import mongoose from 'mongoose'
-import cors from 'cors'
+import express from "express"
+import bodyParser from "body-parser"
+import mongoose from "mongoose"
+import cors from "cors"
+import "dotenv/config"
+const app = express()
+
+app.use(bodyParser.json({ limit: "30mb", extended: true }))
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }))
+app.use(cors())
+
+// https://mongodb.com/cloud/atlas
+const CONNECTION_URL =
+  "mongodb+srv://arjundubey:tacMP15@cluster0.nhyxa.mongodb.net/?retryWrites=true&w=majority"
+
+const PORT = process.env.PORT || 5000
+
+mongoose
+  .connect(CONNECTION_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() =>
+    app.listen(PORT, () => {
+      console.log(`Server running on port : ${PORT}`)
+    })
+  )
+  .catch((error) => console.error(error.message))
+
+//mongoose.set("useFindAndModify", false)
